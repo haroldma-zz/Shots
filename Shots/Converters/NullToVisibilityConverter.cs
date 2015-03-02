ae96771ac5f8a98b0144469334c65d36f3ea92a4
Harry <harry@zumicts.com>
2015-03-02 18:38:03 -0400

    Wire like button to change based on state

--- Shots/Converters/NullToVisibilityConverter.cs
+++ Shots/Converters/NullToVisibilityConverter.cs
@@ -4,7 +4,7 @@ using Windows.UI.Xaml.Data;
 
 namespace Shots.Converters
 {
-    public class NullToVisibilityConverters : IValueConverter
+    public class NullToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
